4fd9c906f9201d887582c76d8b9d8393bd0a0f1a
lwz <905328134@qq.com>
2023-04-27 20:30:10 +0800

    initial

--- run_all_all.sh
+++ run_all_all.sh
@@ -65,6 +65,3 @@ python train_all.py \
   --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/RAF-DB \
   --ckpt_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/ISSL-mocov2plus/experiments/2023_04_17_13_10_58-finetune
 
-
-#/mnt/mmtech01/usr/liuwenzhuo/code/test-code/ISSL-cka-fixbug/experiments/2023_04_13_12_06_58-reparam-fixbn-1x3/1r8ki6ns/reparam-fixbn-1x3-task4-ep=500-1r8ki6ns.ckpt
-#/mnt/mmtech01/usr/liuwenzhuo/code/test-code/ISSL-cka/experiments/2023_04_12_02_58_10-reparam-fixbn-1x3/w66mgepg/reparam-fixbn-1x3-task4-ep=499-w66mgepg.ckpt
