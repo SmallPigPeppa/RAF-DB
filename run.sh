a8ed24166085572ff3b5c116f050f5dcf6837918
lwz <905328134@qq.com>
2023-04-27 17:15:21 +0800

    initial

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-python train.py --max_epochs 30 --batch_size 128 --lr 0.1 --data_path path/to/train --ckpt_path path/to/test
-
+python train.py \
+  --max_epochs 30 \
+  --batch_size 128 \
+  --lr 0.1 \
+  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/RAF-DB \
+  --ckpt_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/ISSL-cka/experiments/2023_04_12_02_58_10-reparam-fixbn-1x3/w66mgepg/reparam-fixbn-1x3-task4-ep=499-w66mgepg.ckpt
