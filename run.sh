b5a8f670f4e329638c366d988f30a527f204fa59
lwz <905328134@qq.com>
2023-04-27 18:48:24 +0800

    initial

--- run.sh
+++ run.sh
@@ -3,4 +3,8 @@ python train.py \
   --batch_size 128 \
   --lr 0.1 \
   --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/RAF-DB \
-  --ckpt_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/ISSL-cka/experiments/2023_04_12_02_58_10-reparam-fixbn-1x3/w66mgepg/reparam-fixbn-1x3-task4-ep=499-w66mgepg.ckpt
+  --ckpt_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/ISSL-cka-fixbug/experiments/2023_04_13_12_06_58-reparam-fixbn-1x3/1r8ki6ns/reparam-fixbn-1x3-task4-ep=500-1r8ki6ns.ckpt
+
+
+#/mnt/mmtech01/usr/liuwenzhuo/code/test-code/ISSL-cka-fixbug/experiments/2023_04_13_12_06_58-reparam-fixbn-1x3/1r8ki6ns/reparam-fixbn-1x3-task4-ep=500-1r8ki6ns.ckpt
+#/mnt/mmtech01/usr/liuwenzhuo/code/test-code/ISSL-cka/experiments/2023_04_12_02_58_10-reparam-fixbn-1x3/w66mgepg/reparam-fixbn-1x3-task4-ep=499-w66mgepg.ckpt
