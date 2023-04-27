c94c3a88d6a71f6a5da08ce80bebed257c604772
lwz <905328134@qq.com>
2023-04-27 22:19:15 +0800

    initial

---
+++ run_all_joint.sh
@@ -0,0 +1,7 @@
+python train_all_joint.py \
+  --max_epochs 50 \
+  --batch_size 128 \
+  --lr 0.1 \
+  --run_name joint \
+  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/RAF-DB \
+  --ckpt_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/RAF-DB/pretrained
